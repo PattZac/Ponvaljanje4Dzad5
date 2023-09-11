372f909a21ba54d1ae0a49a1c2a784b2e4f4b19e
Pat <113007904+PattZac@users.noreply.github.com>
2023-09-11 11:09:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponvaljanje4Dzad5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi URL adresu: ");
+            string link=Console.ReadLine();
+            if (link.Substring(0,4)=="www."&&link.Substring(link.Length-3,3)==".hr")
+            {
+                Console.WriteLine("URL adresu je pravilan.");
+            }
+            else
+            {
+                Console.WriteLine("URL adresu nije pravilan.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
